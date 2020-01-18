141e6813ca26be711c360c8535025971bd03f348
Matheus Fidelis <matheus@nanoshots.com.br>
2020-01-18 15:16:35 -0300

    README

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ func main() {
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
+			"status":  200,
 		})
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
