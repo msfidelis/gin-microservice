ee7a11aef44d239a7e12561a3a3a9e208eaf6721
Matheus Fidelis <matheus@nanoshots.com.br>
2020-01-18 15:11:43 -0300

    refactoring on endpoints

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("EAE")
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
