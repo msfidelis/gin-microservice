8f9a52a6e3118f99be987196179a14640ba7a91c
Matheus Fidelis <matheus@nanoshots.com.br>
2020-01-19 11:11:56 -0300

    redis

--- pkg/controllers/access/access.go
+++ pkg/controllers/access/access.go
@@ -1,11 +1,25 @@
 package access
 
 import "github.com/gin-gonic/gin"
+import "github.com/go-redis/redis"
 
 // GET /ping
 func Ping(c *gin.Context) {
+
+	client := redis.NewClient(&redis.Options{
+		Addr:     "redis:6379",
+		Password: "",
+		DB:       0,
+	})
+
+	result, err := client.Incr("counter").Result()
+	if err != nil {
+		panic(err)
+	}
+
 	c.JSON(200, gin.H{
 		"message": "pong",
+		"access":  result,
 		"status":  200,
 	})
 }
